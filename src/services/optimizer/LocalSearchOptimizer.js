195a21d4e66b3e8fbe34adb72645d565833519d9
Andrey Popov <avpv@users.noreply.github.com>
2025-10-30 07:43:07 +0100

    Create LocalSearchOptimizer.js

---
+++ src/services/optimizer/LocalSearchOptimizer.js
@@ -0,0 +1,62 @@
+// src/services/optimizer/LocalSearchOptimizer.js
+
+import IOptimizer from './IOptimizer.js';
+import { cloneTeams } from './utils/solutionUtils.js';
+import { performUniversalSwap } from './utils/swapOperations.js';
+
+/**
+ * Local Search Optimizer
+ * Performs hill-climbing to refine an existing solution
+ */
+class LocalSearchOptimizer extends IOptimizer {
+    constructor(config, adaptiveParams) {
+        super(config);
+        this.adaptiveParams = adaptiveParams;
+        this.stats = {
+            iterations: 0,
+            improvements: 0
+        };
+    }
+
+    /**
+     * Solve using Local Search (hill climbing)
+     * @param {Object} problemContext - Problem context
+     * @returns {Promise<Array>} Best solution found
+     */
+    async solve(problemContext) {
+        const {
+            initialSolution,
+            positions,
+            evaluateFn
+        } = problemContext;
+
+        let current = cloneTeams(initialSolution);
+        let currentScore = evaluateFn(current);
+        
+        for (let iter = 0; iter < this.config.iterations; iter++) {
+            this.stats.iterations = iter + 1;
+            
+            const neighbor = cloneTeams(current);
+            performUniversalSwap(neighbor, positions, this.adaptiveParams);
+            const neighborScore = evaluateFn(neighbor);
+            
+            // Accept only improvements (greedy hill climbing)
+            if (neighborScore < currentScore) {
+                current = neighbor;
+                currentScore = neighborScore;
+                this.stats.improvements++;
+            }
+            
+            // Yield control periodically
+            if (iter % 100 === 0) await new Promise(resolve => setTimeout(resolve, 1));
+        }
+        
+        return current;
+    }
+
+    getStatistics() {
+        return this.stats;
+    }
+}
+
+export default LocalSearchOptimizer;
